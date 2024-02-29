01d59ddc314f5323bdbc66f02ac41be798d1a1da
Renato Gava <renatogava2@live.com>
2024-02-28 23:06:53 -0300

    funcionalidades dos componentes

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Task } from '../models/task';
+import { TaskService } from '../services/task.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
 export class TaskDetailsComponent {
 
+  task?: Task;
+
+  constructor(private router: Router, private route: ActivatedRoute, private taskService: TaskService) { }
+
+  ngOnInit() {
+
+    let id = this.route.snapshot.paramMap.get('id');
+    
+    if (id === null) {
+      this.navigateBack();
+    }
+    else
+    {
+      this.task = this.taskService.getById(+id);
+
+      if (this.task === undefined) {
+        this.navigateBack();
+      }
+    }
+  }
+
+  save() {
+    this.taskService.updateTasks();
+
+    this.navigateBack();
+  }
+
+  cancel() {
+    this.navigateBack();
+  }
+
+  private navigateBack() {
+
+    this.router.navigate(['/taskList'], { relativeTo: this.route});
+  }
 }
--- src/app/task-filter/task-filter.component.ts
+++ src/app/task-filter/task-filter.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-filter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-filter.component.html',
   styleUrl: './task-filter.component.css'
 })
 export class TaskFilterComponent {
 
+  filter: string = '';
+
+  @Output() taskFilter = new EventEmitter<string>();
+
+  filterTask() {
+    this.taskFilter.emit(this.filter);
+  }
 }
