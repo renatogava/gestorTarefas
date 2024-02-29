987e4b48ae174ed76b37abcda7758bad71439216
Renato Gava <renatogava2@live.com>
2024-02-28 21:44:40 -0300

    criando html dos componentes

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TaskFilterComponent } from "../task-filter/task-filter.component";
 
 @Component({
-  selector: 'app-task-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.css'
+    selector: 'app-task-list',
+    standalone: true,
+    templateUrl: './task-list.component.html',
+    styleUrl: './task-list.component.css',
+    imports: [TaskFilterComponent]
 })
 export class TaskListComponent {
 
